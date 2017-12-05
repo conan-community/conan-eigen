31307f388d93f061fee6c2f57615994ba6df5c2a
danimtb <danimanzaneque@gmail.com>
2017-12-05 18:28:50 +0100

    Adds build_policy

--- conanfile.py
+++ conanfile.py
@@ -11,12 +11,12 @@ class EigenConan(ConanFile):
     url = "http://eigen.tuxfamily.org"
     description = "Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms."
     license = "http://eigen.tuxfamily.org/index.php?title=Main_Page#License"
+    build_policy = "always"
 
     def source(self):
         source_url = "https://bitbucket.org/eigen/eigen"
         tools.get("{0}/get/{1}.tar.gz".format(source_url, self.version))
         os.rename(glob("eigen-eigen-*")[0], "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def package(self):
         self.copy(pattern="*", dst="include/Eigen", src="sources/Eigen")
