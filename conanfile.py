a93f1cbc4ecf44a526db79af1a80beaee65c7f96
danimtb <danimanzaneque@gmail.com>
2017-12-07 19:37:12 +0100

    Added homepage

--- conanfile.py
+++ conanfile.py
@@ -8,15 +8,15 @@ from glob import glob
 
 class EigenConan(ConanFile):
     name = "eigen"
-    url = "http://eigen.tuxfamily.org"
+    url = "https://bitbucket.org/eigen/eigen"
+    homepage = "http://eigen.tuxfamily.org"
     description = "Eigen is a C++ template library for linear algebra: matrices, vectors, \
                    numerical solvers, and related algorithms."
     license = "Mozilla Public License Version 2.0"
     no_copy_source = True
 
     def source(self):
-        source_url = "https://bitbucket.org/eigen/eigen"
-        tools.get("{0}/get/{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/get/{1}.tar.gz".format(self.url, self.version))
         os.rename(glob("eigen-eigen-*")[0], "sources")
 
     def package(self):
