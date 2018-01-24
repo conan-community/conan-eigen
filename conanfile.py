a1a4a5e874cccd4f032ee35a8024957d93a56b4b
danimtb <danimanzaneque@gmail.com>
2018-01-24 10:14:51 +0100

    version 3.1.4

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from glob import glob
 
 class EigenConan(ConanFile):
     name = "eigen"
-    version = "3.3.4"
+    version = "3.1.4"
     url = "https://bitbucket.org/eigen/eigen"
     homepage = "http://eigen.tuxfamily.org"
     description = "Eigen is a C++ template library for linear algebra: matrices, vectors, \
