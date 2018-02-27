a6b976b593a29465bc855c2561c25abd0d26b3fa
danimtb <danimanzaneque@gmail.com>
2018-02-27 11:41:08 +0100

    Updated license, readme and build script. Removed Travis

--- README.md
+++ README.md
@@ -27,30 +27,6 @@ If you handle multiple dependencies in your project is better to add a *conanfil
     txt
     cmake
 
-Complete the installation of requirements for your project running:
-
-    $ mkdir build && cd build && conan install ..
-
-**Note:** It is recommended that you run conan install from a build directory and not the root of the project directory.  This is because conan generates *conanbuildinfo* files specific to a single build configuration which by default comes from an autodetected default profile located in ~/.conan/profiles/default .  If you pass different build configuration options to conan install, it will generate different *conanbuildinfo* files.  Thus, they should not be added to the root of the project, nor committed to git.
-
-## For Packagers: Publish this Package
-
-The example below shows the commands used to publish to `conan-community` conan repository. To publish to your own conan respository (for example, after forking this git repository), you will need to change the commands below accordingly.
-
-## Build and package
-
-The following command both runs all the steps of the conan file, and publishes the package to the local system cache.  This includes downloading dependencies from `build_requires` and `requires` , and then running the `build()` method.
-
-    $ conan create conan/stable
-
-## Add Remote
-
-    $ conan remote add conan-community "https://api.bintray.com/conan/conan-community/conan"
-
-## Upload
-
-    $ conan upload eigen/3.1.4@conan/stable --all -r conan-community
-
 ## License
 
 [MIT License](LICENSE)
--- build.py
+++ build.py
@@ -14,36 +14,12 @@ def get_name_from_recipe():
 def get_version_from_recipe():
     return get_value_from_recipe(r'''version\s*=\s*["'](\S*)["']''').groups()[0]
 
-def get_default_vars():
-    username = os.getenv("CONAN_USERNAME", "conan")
-    channel = os.getenv("CONAN_CHANNEL", "stable")
-    version = get_version_from_recipe()
-    return username, channel, version
-
-def is_ci_running():
-    return os.getenv("APPVEYOR_REPO_NAME","") or os.getenv("TRAVIS_REPO_SLUG","")
-
-def get_ci_vars():
-    reponame_a = os.getenv("APPVEYOR_REPO_NAME","")
-    repobranch_a = os.getenv("APPVEYOR_REPO_BRANCH","")
-
-    reponame_t = os.getenv("TRAVIS_REPO_SLUG","")
-    repobranch_t = os.getenv("TRAVIS_BRANCH","")
-
-    username, _ = reponame_a.split("/") if reponame_a else reponame_t.split("/")
-    channel, version = repobranch_a.split("/") if repobranch_a else repobranch_t.split("/")
-    return username, channel, version
-
-def get_env_vars():
-    return get_ci_vars() if is_ci_running() else get_default_vars()
-
-def get_os():
-    return platform.system().replace("Darwin", "Macos")
-
 if __name__ == "__main__":
     name = get_name_from_recipe()
-    username, channel, version = get_default_vars()
-    login_username = os.getenv("CONAN_LOGIN_USERNAME", "danimtb")
+    username = "conan"
+    channel = "stable"
+    version = get_version_from_recipe()
+    login_username = "conanbot"
     reference = "{0}/{1}".format(name, version)
     upload_remote = "https://api.bintray.com/conan/conan-community/{0}".format(username)
 
@@ -54,6 +30,11 @@ if __name__ == "__main__":
         reference=reference,
         upload=upload_remote,
         remotes=upload_remote)
-
-    builder.add_common_builds()
+    builder.add(settings={"os":"windows",
+                          "compiler":"Visual Studio",
+                          "compiler.version":"15",
+                          "build_type":"Release",
+                          "arch":"x86_64"},
+                options=None,
+                env_vars=None)
     builder.run()
